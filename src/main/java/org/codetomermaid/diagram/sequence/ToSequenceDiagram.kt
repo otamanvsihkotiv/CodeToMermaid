17d766a4d50840c5b12a21e733d40c29be99137c
Antonina <antonibu@uio.no>
2025-08-27 22:39:24 +0200

    chore: move all traversing logic to parseAndDescribe

--- src/main/java/org/codetomermaid/diagram/sequence/ToSequenceDiagram.kt
+++ src/main/java/org/codetomermaid/diagram/sequence/ToSequenceDiagram.kt
@@ -1,7 +1,6 @@
 package org.codetomermaid.diagram.sequence
 
 import com.github.javaparser.StaticJavaParser
-import org.codetomermaid.dto.ParsedMethod
 import org.codetomermaid.javaparser.SeqDVisitor
 import org.codetomermaid.javaparser.createTypeResolver
 import java.nio.file.Files
@@ -14,35 +13,18 @@ fun toSequenceDiagram(
     genFileName: String
 ) {
     createTypeResolver(sourceFolder)
-    val visitor = SeqDVisitor()
-
-    // parse entry file
-    val entryFileCU = StaticJavaParser.parse(filePath)
-    entryFileCU.accept(visitor, null)
-    val parsedFileEntry = visitor.parsedFile
-
-    // find entry method
-    val methods = parsedFileEntry.methods
-    val pm: ParsedMethod? = methods.find { it.name == entryMethod }
-    requireNotNull(pm) {
-        "No method named $entryMethod in class ${parsedFileEntry.className}.\n" +
-                "Methods found in class ${parsedFileEntry.className}: ${methods.forEach { println(it.name) }}"
-    }
 
     val lines = mutableListOf<String>()
     lines.add("```mermaid")
     lines.add("sequenceDiagram")
 
-    pm.methodCalls.forEach {
-        if (it.pathToClass != null) {
-            parseAndAppend(
-                it.pathToClass,
-                it.name,
-                callerClassName = parsedFileEntry.className ?: "WTF",
-                lines = lines,
-            )
-        }
-    }
+    parseAndDescribe(
+        path = filePath,
+        targetMethod = entryMethod,
+        callerClassName = null,
+        lines = lines,
+    )
+
     lines.add("```")
 
     // create file
@@ -52,37 +34,46 @@ fun toSequenceDiagram(
     Files.write(saveTo, lines)
 }
 
-fun parseAndAppend(
+private fun parseAndDescribe(
     path: Path,
     targetMethod: String,
-    callerClassName: String,
-    lines: MutableList<String>
+    callerClassName: String?,
+    lines: MutableList<String>,
 ) {
-    // parse next file and save it to visited so we don't have to parse it again (just in case)
+    // parse the file defined by path
     val visitor = SeqDVisitor()
     StaticJavaParser.parse(path).accept(visitor, null)
     val parsedFile = visitor.parsedFile
+    val className = parsedFile.className
 
-    // describe call from prev class to this class
-    lines.add("$TAB$callerClassName $MESSAGE_ARROW ${parsedFile.className}: $targetMethod()")
+    // describe call from prev class to this class if not the entry method
+    if (callerClassName != null) {
+        lines.add("$TAB$callerClassName $MESSAGE_ARROW ${className}: $targetMethod()")
+    }
 
     // find method decl and look through called methods in it
     val method = parsedFile.methods.find { it.name == targetMethod }
-    requireNotNull(method) { "$targetMethod not found in ${parsedFile.className}" }
+
+    requireNotNull(method) {
+        "No method named $targetMethod in class ${className}.\n" +
+                "Methods found in class ${className}: ${parsedFile.methods.forEach { println(it.name) }}"
+    }
 
     // continue recursively until we find a method that doesn't call any (known) methods
     method.methodCalls.forEach {
         if (it.pathToClass != null) {
-            parseAndAppend(
+            parseAndDescribe(
                 it.pathToClass,
                 it.name,
-                callerClassName = parsedFile.className ?: "???",
+                callerClassName = className ?: "???",
                 lines = lines,
             )
         }
     }
 
     // describe reply messages
-    lines.add("$TAB${parsedFile.className} $RETURN_ARROW $callerClassName: ${method.returnType}")
+    if (callerClassName != null) {
+        lines.add("$TAB${parsedFile.className} $RETURN_ARROW $callerClassName: ${method.returnType}")
+    }
 
 }
